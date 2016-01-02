323626ceeb91e411d27e8cd71da67b0f814b9606
david <david@DESKTOP-VCKUD1C>
2016-01-01 23:14:18 -0500

    Correcto

---
+++ FoodBet-ejb/src/main/java/com/espe/distribuidas/foodbet/servicios/EquipoEventoServicio.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.espe.distribuidas.foodbet.servicios;
+
+import com.espe.distribuidas.foodbet.dao.EventoEquipoDAO;
+import com.espe.distribuidas.foodbet.modelo.EventoEquipo;
+import com.espe.distribuidas.foodbet.modelo.EventoEquipoPK;
+import java.util.List;
+import javax.ejb.EJB;
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author david
+ */
+@LocalBean
+@Stateless
+public class EquipoEventoServicio {
+    
+    @EJB
+    private EventoEquipoDAO eventEquipoDAO;
+    
+    public List<EventoEquipo> obtenerTodosEventoEquipo(){
+        return this.eventEquipoDAO.findAll();
+    }
+    
+    public EventoEquipo obtenerEventoEquipoPorID(EventoEquipoPK ee){
+        return this.eventEquipoDAO.findById(ee, false);
+    }
+    
+    public void ingresarEventoEquipo(EventoEquipo ee){
+        this.eventEquipoDAO.insert(ee);
+    }
+    
+    public void actualizatEventoEquipo(EventoEquipo ee){
+        this.eventEquipoDAO.update(ee);
+    }
+}
